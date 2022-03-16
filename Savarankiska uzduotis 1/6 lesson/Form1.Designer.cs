d6f06c0f5242d00caa642ca08fdde4a219c08a6d
DaumaG <dgarnys18@gmail.com>
2022-03-16 19:38:55 +0200

    6 lesson - savarankiskas darbas 01

---
+++ Savarankiska uzduotis 1/6 lesson/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace _6_lesson
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameInputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConsoleTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConfirmationButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameInputTextBox
+            // 
+            this.NameInputTextBox.Location = new System.Drawing.Point(94, 33);
+            this.NameInputTextBox.Name = "NameInputTextBox";
+            this.NameInputTextBox.Size = new System.Drawing.Size(100, 23);
+            this.NameInputTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jusu vardas";
+            // 
+            // ConsoleTextBox
+            // 
+            this.ConsoleTextBox.Location = new System.Drawing.Point(368, 33);
+            this.ConsoleTextBox.Multiline = true;
+            this.ConsoleTextBox.Name = "ConsoleTextBox";
+            this.ConsoleTextBox.ReadOnly = true;
+            this.ConsoleTextBox.Size = new System.Drawing.Size(151, 103);
+            this.ConsoleTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Konsolė";
+            // 
+            // ConfirmationButton
+            // 
+            this.ConfirmationButton.Location = new System.Drawing.Point(94, 62);
+            this.ConfirmationButton.Name = "ConfirmationButton";
+            this.ConfirmationButton.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmationButton.TabIndex = 4;
+            this.ConfirmationButton.Text = "Patvirtinti";
+            this.ConfirmationButton.UseVisualStyleBackColor = true;
+            this.ConfirmationButton.Click += new System.EventHandler(this.ConfirmationButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ConfirmationButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ConsoleTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NameInputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameInputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ConsoleTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ConfirmationButton;
+    }
+}
---
+++ Savarankiska uzduotis 1/6 lesson/OutputWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_lesson
+{
+    internal class OutputWriter
+    {
+        public void WriteName(string name, TextBox outputTextBox)
+        {
+            outputTextBox.Text = $"Labas, {name}";
+        }
+    }
+}
