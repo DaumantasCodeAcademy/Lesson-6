4cda3ff2e43f0018496a9ca851506b897fb42522
DaumaG <dgarnys18@gmail.com>
2022-03-16 21:44:01 +0200

    Savarankiskas darbas (2) - 3

---
+++ Savarankiska uzduotis 1/Savarankiskas darbas 2-3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Savarankiskas_darbas_2_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PowerInputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NumberInputTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PowerInputTextBox
+            // 
+            this.PowerInputTextBox.Location = new System.Drawing.Point(199, 58);
+            this.PowerInputTextBox.Name = "PowerInputTextBox";
+            this.PowerInputTextBox.Size = new System.Drawing.Size(100, 23);
+            this.PowerInputTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Iveskite laipsni";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Iveskite skaiciu";
+            // 
+            // NumberInputTextBox
+            // 
+            this.NumberInputTextBox.Location = new System.Drawing.Point(199, 24);
+            this.NumberInputTextBox.Name = "NumberInputTextBox";
+            this.NumberInputTextBox.Size = new System.Drawing.Size(100, 23);
+            this.NumberInputTextBox.TabIndex = 3;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(199, 97);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(100, 23);
+            this.CalculateButton.TabIndex = 4;
+            this.CalculateButton.Text = "Suskaiciuoti";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(430, 24);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.ReadOnly = true;
+            this.ResultTextBox.Size = new System.Drawing.Size(100, 23);
+            this.ResultTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rezultatas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.NumberInputTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PowerInputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PowerInputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NumberInputTextBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Savarankiska uzduotis 1/Savarankiskas darbas 2-3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Savarankiskas_darbas_2_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(NumberInputTextBox.Text);
+            int power = Convert.ToInt32(PowerInputTextBox.Text);
+
+            double result = Math.Pow(number, power);
+            ResultTextBox.Text = result.ToString();
+        }
+    }
+}
